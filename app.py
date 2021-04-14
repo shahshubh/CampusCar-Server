7edd94356df5d2fdd077b8de9bbf285f736b0cff
shubh <shahshubh1010@gmail.com>
2021-04-14 16:04:27 +0530

    added ngrok

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
+from flask_ngrok import run_with_ngrok
 from flask import Flask,request,jsonify
 import cv2
-# from matplotlib import pyplot as plt
-from PIL import Image
 import pytesseract
-
 import imutils
 import numpy as np
 import os
-import glob
 
-app=Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
-# pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+
+app=Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/')
 def welcome():
-    # img = cv2.imread("./images/vietnam_car_rectangle_plate.jpg");
-    # text = pytesseract.image_to_string(img, config='--psm 13')
     return jsonify(welcome = "Welcome to Campus Car Server");
 
 @app.route("/upload",methods=['POST'])
@@ -29,7 +25,7 @@ def upload():
         print("Couldn't create upload directory: {}".format(target))
 
     if request.files:
-        img = request.files["image"]
+        img = request.files["upload"]
         filename = img.filename
 
         destination = "/".join([target, filename])
@@ -69,16 +65,11 @@ def upload():
             (topx, topy) = (np.min(x), np.min(y))
             (bottomx, bottomy) = (np.max(x), np.max(y))
             Cropped = gray[topx:bottomx+1, topy:bottomy+1]
-
             # new_cropped, img_bin = cv2.threshold(Cropped,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
             # new_cropped = cv2.bitwise_not(img_bin)
             # cv2.imwrite(destination, new_cropped)
-
-
             text = pytesseract.image_to_string(Cropped, config='--psm 11', lang='eng')
             text = text.strip()
-
-            print("Before Detected license plate Number is:",text)
 
             getVals = list([val for val in text if val.isalpha() or val.isnumeric()]) 
             text = "".join(getVals)
@@ -91,5 +82,4 @@ def upload():
         return jsonify({'success': False,'error': "Please send some Image File"});
 
 if __name__=='__main__':
-    app.run(debug=True,host="localhost",port='3000')
-    # 192.168.0.105
+    app.run()
